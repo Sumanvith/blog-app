332308d812fa5c75f98e0224af2a159a8fd2e42d
Sumanvith <dellbilla@gmail.com>
2025-04-08 20:50:16 +0530

    input box

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Test First react app case", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = screen.getByText(/First React Test Case/i);
+  const title = screen.getByTitle(/photo/i);
+  expect(text).toBeInTheDocument();
+  expect(title).toBeInTheDocument();
+});
+
+test("Testing input box", () => {
+  render(<App />);
+  const checkInput = screen.getByRole("textbox");
+  const checkInputPlaceHolder = screen.getByPlaceholderText("Enter User Name");
+  expect(checkInput).toBeInTheDocument();
+  expect(checkInput).toHaveAttribute("name", "username");
+  expect(checkInput).toHaveAttribute("id", "userId");
+  expect(checkInputPlaceHolder).toBeInTheDocument();
 });
