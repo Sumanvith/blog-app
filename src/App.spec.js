819ce96ddad4926f03dd24aa18fe030e01365637
Sumanvith <dellbilla@gmail.com>
2025-04-10 20:52:49 +0530

    before and after hooks

--- src/App.spec.js
+++
@@ -1,9 +0,0 @@
-import { fireEvent, render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("onChange event testing", () => {
-  render(<App />);
-  const btn = screen.getByRole("button");
-  fireEvent.click(btn);
-  expect(screen.getByText("updated data")).toBeInTheDocument();
-});
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,28 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("onChange event testing", () => {
+beforeAll(() => {
+  console.log(1234);
+});
+beforeEach(() => {
+  console.log(1234);
+});
+
+afterAll(() => {
+  console.log(1234);
+});
+afterEach(() => {
+  console.log(1234);
+});
+
+test("onChange event testing 1", () => {
+  render(<App />);
+  const btn = screen.getByRole("button");
+  fireEvent.click(btn);
+  expect(screen.getByText("updated data")).toBeInTheDocument();
+});
+
+test("onChange event testing 2", () => {
   render(<App />);
   const btn = screen.getByRole("button");
   fireEvent.click(btn);
