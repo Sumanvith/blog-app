6b0f5974381dff232e282ec485762ce3e3d39b69
Sumanvith <dellbilla@gmail.com>
2025-04-10 11:38:56 +0530

    test extensions and folder

---
+++ src/App.test.jsx
@@ -0,0 +1,9 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("onChange event testing", () => {
+  render(<App />);
+  const btn = screen.getByRole("button");
+  fireEvent.click(btn);
+  expect(screen.getByText("updated data")).toBeInTheDocument();
+});
