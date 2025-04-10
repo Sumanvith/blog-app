cc7b8167476dff587f8eeebfea1c985501071158
Sumanvith <dellbilla@gmail.com>
2025-04-10 21:12:28 +0530

    snapshot

--- src/App.test.js
+++ src/App.test.js
@@ -1,30 +1,7 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
 
-beforeAll(() => {
-  console.log(1234);
-});
-beforeEach(() => {
-  console.log(1234);
-});
-
-afterAll(() => {
-  console.log(1234);
-});
-afterEach(() => {
-  console.log(1234);
-});
-
-test("onChange event testing 1", () => {
-  render(<App />);
-  const btn = screen.getByRole("button");
-  fireEvent.click(btn);
-  expect(screen.getByText("updated data")).toBeInTheDocument();
-});
-
-test("onChange event testing 2", () => {
-  render(<App />);
-  const btn = screen.getByRole("button");
-  fireEvent.click(btn);
-  expect(screen.getByText("updated data")).toBeInTheDocument();
+test("snapshot for app component", () => {
+  const container = render(<App />);
+  expect(container).toMatchSnapshot();
 });
