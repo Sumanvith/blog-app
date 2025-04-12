14c312d72303f70e80aff0109de681644513a7f9
Sumanvith <dellbilla@gmail.com>
2025-04-12 23:16:08 +0530

    getbyallrole

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Multiple Item with Role</h1>
-      <h1>Custom Role</h1>
+      <h1>getAllByRole</h1>
       <button>Click 1</button>
       <button>Click 2</button>
-      <label htmlFor="input1">Username</label>
-      <input type="text" id="input1"></input>
-      <label htmlFor="input2">User age</label>
-      <input type="text" id="input2"></input>
-      <div role="dummy">dummy text</div>
+      <select>
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+      </select>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,14 @@
-import { render, screen } from "@testing-library/react";
+import { getRoles, render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("get by role", () => {
+test("getAllByRole", () => {
   render(<App />);
-  const btn1 = screen.getByRole("button", { name: "Click 1" });
-  const btn2 = screen.getByRole("button", { name: "Click 2" });
-  const input1 = screen.getByRole("textbox", { name: "Username" });
-  const input2 = screen.getByRole("textbox", { name: "User age" });
-  const dv1 = screen.getByRole("dummy");
-  expect(btn1).toBeInTheDocument();
-  expect(btn2).toBeInTheDocument();
-  expect(input1).toBeInTheDocument();
-  expect(input2).toBeInTheDocument();
-  expect(dv1).toBeInTheDocument();
+  const btns = screen.getAllByRole("button");
+  const options = screen.getAllByRole("option");
+  for (let i = 0; i < btns.length; i++) {
+    expect(btns[i]).toBeInTheDocument();
+  }
+  for (let i = 0; i < options.length; i++) {
+    expect(options[i]).toBeInTheDocument();
+  }
 });
