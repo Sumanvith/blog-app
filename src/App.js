850f31471eecf38c4bd757facca7eb93dc6fc471
Sumanvith <dellbilla@gmail.com>
2025-04-13 15:42:54 +0530

    getAllByLabelText

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>getByLabelText</h1>
-      <label htmlFor="user-name">Username</label>
-      <input type="text" id="user-name"></input>
+      <h1>getAllByLabelText</h1>
+      <label htmlFor="user-name1">Username</label>
+      <input type="text" id="user-name1" defaultValue={"hello"}></input>
 
-      <label htmlFor="skills">Skills</label>
-      <input type="checkbox" id="skills" defaultChecked={true}></input>
+      <label htmlFor="user-name2">Username</label>
+      <input type="text" id="user-name2" defaultValue={"hello"}></input>
+
+      <label htmlFor="user-name3">Username</label>
+      <input type="text" id="user-name3" defaultValue={"hello"}></input>
+
+      <label htmlFor="skill1">Skills</label>
+      <input type="checkbox" id="skill1" defaultChecked={true}></input>
+
+      <label htmlFor="skill2">Skills</label>
+      <input type="checkbox" id="skill2" defaultChecked={true}></input>
+
+      <label htmlFor="skill3">Skills</label>
+      <input type="checkbox" id="skill3" defaultChecked={true}></input>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("getByLabelText 1", () => {
+test("getAllByLabelText input", () => {
   render(<App />);
-  const input = screen.getByLabelText("Username");
-  expect(input).toBeInTheDocument();
+  const inputs = screen.getAllByLabelText("Username");
+  for (let i = 0; i < inputs.length; i++) {
+    expect(inputs[i]).toBeInTheDocument();
+    expect(inputs[i]).toHaveValue("hello");
+  }
 });
 
-test("getByLabelText 2", () => {
+test("getAllByLabelText checkbox", () => {
   render(<App />);
-  const checkbox = screen.getByLabelText("Skills");
-  expect(checkbox).toBeInTheDocument();
-  expect(checkbox).toBeChecked();
+  const checkboxes = screen.getAllByLabelText("Skills");
+  for (let i = 0; i < checkboxes.length; i++) {
+    expect(checkboxes[i]).toBeInTheDocument();
+    expect(checkboxes[i]).toBeChecked();
+  }
 });
