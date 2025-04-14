d61b5ab7b69dd46a82056020dfadd81852483ec1
Sumanvith <dellbilla@gmail.com>
2025-04-14 20:38:41 +0530

    queryBy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 
 function App() {
+  let login = false;
   return (
     <div className="App">
-      <h1>TextMatch with Function</h1>
-      <div>Hello World</div>
+      <h1>queryBy</h1>
+      {login ? <button>Logout</button> : <button>Login</button>}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,22 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("text match with function", () => {
+test("queryBy", () => {
   render(<App />);
-  const div1 = screen.getByText((content, element) =>
-    content.startsWith("Hello")
-  );
-  const div2 = screen.getByText((content, element) =>
-    content.endsWith("World")
-  );
-  const div3 = screen.getByText((content, element) =>
-    content.includes("lo Wo")
-  );
-  const div4 = screen.getByText((content, element) => {
-    return content.length === 11;
-  });
+  const div1 = screen.queryByText("Login");
+  const div2 = screen.queryByText("Logout");
   expect(div1).toBeInTheDocument();
-  expect(div2).toBeInTheDocument();
-  expect(div3).toBeInTheDocument();
-  expect(div4).toBeInTheDocument();
+  expect(div2).not.toBeInTheDocument();
 });
