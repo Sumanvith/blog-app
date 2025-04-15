cd48f6c43a4bdca3e8b3e8b43684e5cfbaf6883f
Sumanvith <dellbilla@gmail.com>
2025-04-15 12:17:39 +0530

    Act Function

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,13 @@
-import { render, screen } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
 import App from "./App";
 import userEvent from "@testing-library/user-event";
 
-test("onChange Event Testing", async () => {
+test("Act Function", async () => {
   userEvent.setup();
   render(<App />);
   const el = screen.getByRole("textbox");
-  await userEvent.type(el, "hello");
+  await act(async () => {
+    await userEvent.type(el, "hello");
+  });
   expect(screen.getByText("hello")).toBeInTheDocument();
 });
