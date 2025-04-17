b043f11b27fbf5edbc5f983f90e583fd13d0319d
Sumanvith <dellbilla@gmail.com>
2025-04-17 11:38:42 +0530

    Props Testing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import { useState } from "react";
 import "./App.css";
+import User from "./User";
 
 function App() {
-  const [name, setName] = useState("");
   return (
     <div className="App">
-      <h1>onChange Event Testing</h1>
-      <h2>{name}</h2>
-      <input
-        type="text"
-        placeholder="enter name"
-        onChange={(e) => {
-          setName(e.target.value);
-        }}></input>
+      <h1>Props Testing</h1>
+      <User name="Hello"></User>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,8 @@
-import { act, render, screen } from "@testing-library/react";
-import App from "./App";
-import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
+import User from "./User";
 
-test("Act Function", async () => {
-  userEvent.setup();
-  render(<App />);
-  const el = screen.getByRole("textbox");
-  await act(async () => {
-    await userEvent.type(el, "hello");
-  });
-  expect(screen.getByText("hello")).toBeInTheDocument();
+test("Props Testing", () => {
+  render(<User name="Hello" />);
+  const user = screen.getByText("Hello");
+  expect(user).toBeInTheDocument();
 });
